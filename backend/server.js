1ddbf13b533d620932e56adac94ac02a6e9d4ca4
neriyaMalachi <neriyamalachi@gmail.com>
2023-02-14 15:58:30 +0200

    Revert "beetwin totorial 33"
    This reverts commit c3390835bc874bc45001fa9e0e2f4e1631e3c987.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import path from "path";
+import data from "./data.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
@@ -31,11 +31,6 @@ app.use("/api/propertis", productRoute);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
@@ -43,4 +38,5 @@ app.use((err, req, res, next) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
+  console.log(data.propertis);
 });
