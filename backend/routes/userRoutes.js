415da1d4060007d6814c7e55eab48e67feae629b
Neriya Malachi <neriamalachi@gmail.com>
2024-05-20 16:09:59 +0300

    end day

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -59,7 +59,7 @@ userRouter.post(
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user.isAdmin,
+      isAdmin: true,
       token: generateToken(user),
     });
   })
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors"
+import cors from "cors";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
 import productRoute from "./routes/productRoutes.js";
@@ -9,15 +9,21 @@ import orderRouter from "./routes/orderRoutes.js";
 import reviewRouter from "./routes/reviewRoutes.js";
 dotenv.config();
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // mongoose.connect("mongodb://127.0.0.1:27017/Store_N")
-mongoose.connect(process.env.MONGODB_URI)
-app.listen(3001,()=>{
+
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.MONGODB_URI);
+
+app.listen(3002, () => {
   console.log("new server conect");
-})
+});
+
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -30,19 +36,18 @@ app.use(function (req, res, next) {
 app.get("/api/keys/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
-app.use(express.json())
+app.use(express.json());
 app.use("/api/seed", seedRouter);
 app.use("/api/propertis", productRoute);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 app.use("/api/reviews", reviewRouter);
 
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
+const port =  5000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
 });
